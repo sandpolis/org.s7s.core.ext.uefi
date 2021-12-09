b2c9841ec636ec574bc84f5f173b600d27cabbb6
Tyler Cook <tcc@sandpolis.com>
2021-12-08 21:48:54 -0600

    Initial commit

---
+++ src/main/java/com/sandpolis/core/integration/uefi/linux/Efibootmgr.java
@@ -0,0 +1,21 @@
+//============================================================================//
+//                                                                            //
+//                         Copyright © 2015 Sandpolis                         //
+//                                                                            //
+//  This source file is subject to the terms of the Mozilla Public License    //
+//  version 2. You may not use this file except in compliance with the MPL    //
+//  as published by the Mozilla Foundation.                                   //
+//                                                                            //
+//============================================================================//
+package com.sandpolis.core.agent.uefi.linux;
+
+public class Efibootmgr {
+
+	@Override
+	public boolean isEfiMode() {
+		// Check for mounted efivars
+
+		return false;
+	}
+
+}
